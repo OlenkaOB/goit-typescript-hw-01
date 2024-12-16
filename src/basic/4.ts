dd6c1bb24545910571992c1d401f4a6a0b99dc63
Borodkina Olena <elena.kaminnik@gmail.com>
2024-12-16 23:15:32 +0200

    add install

---
+++ src/basic/4.ts
@@ -0,0 +1,11 @@
+function showMessage(message: string) {
+  console.log(message);
+}
+
+function calc(num1, num2) {
+  return num1 + num2;
+}
+
+function customError() {
+  throw new Error("Error");
+}
