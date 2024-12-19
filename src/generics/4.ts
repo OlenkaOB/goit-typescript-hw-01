c268491ac4f76c5c14cbd6aa254ac1d99c667be4
Borodkina Olena <elena.kaminnik@gmail.com>
2024-12-19 10:11:16 +0200

    add 4.ts

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -1,11 +1,11 @@
-type User = {
+type Users = {
   name: string;
   surname: string;
   email: string;
   password: string;
 };
 
-function createOrUpdateUser(initialValues: Partial<User>): void {
+function createOrUpdateUser(initialValues: Partial<Users>): void {
   // Оновлення користувача
   console.log("User data", initialValues);
 }
